eb094af70343fcb30eef3c4a346ed6227fbbbb77
Matthew S Rich <matt.rich@utah.edu>
2022-07-07 15:34:14 -0600

    added functionality to perform all-by-all intersections

--- intersect_controls.py
+++ intersect_controls.py
@@ -9,24 +9,50 @@ Matt Rich, 8/2021
 
 import os
 
-def main(infile, prefix):
-		for line in open(infile, "r"):
+def main(infile, prefix, isec_all):
+		isec_commands = []
+		out_l_template = "bcftools isec -p {} -C ".format(prefix) + "{}"
+
+		if isec_all:
+				#compile all the unique files to compare
+				all_files = set()
+				for x in open(infile, "r").readlines():
+						for y in x.split("\t"):
+								all_files.add(y)
+				all_files = list(all_files)
+				#then write all the isec commands
+				for i in range(len(all_files)):
+						f_list = " ".join([all_files[i]] + all_files[0:i] + all_files[i+1:])
+						isec_commands.append(out_l_template.format(f_list))
+				print(isec_commands)
+				
+		else:
+			for line in open(infile, "r"):
 				out_l = "bcftools isec -p {} -C ".format(prefix)
 
 				l = line.strip().split("\t")
 				
 				#call bcftools isec
-				out_l += " ".join(l)			
+				isec_commands.append(out_l_template.format(" ".join(l)))		
 
-				print(out_l)
+				
+		#after compiling all isec commands, run them		
+		for out_l in isec_commands:
 				os.system(out_l)
 
-				out_name_parts = l[0].split("/")[1].split(".")
+				out_name_parts = out_l.split(" ")[5].split(".")
 				isec_name = ".".join([out_name_parts[0], 
 							"isec", 
 							".".join(out_name_parts[1:])])
+				#remove any upstream folder names from isec_name
+				try:
+						isec_name = isec_name.split("/")[1]
+				except IndexError:
+						pass
+
 				#rename the output (0000.vcf) to something meaningful
 				os.system("mv {0}/0000.vcf {0}/{1}".format(prefix, isec_name.rstrip(".gz")))
+
 		#then delete the README and sites files
 		os.system("rm {}/README {}/sites.txt")
 
@@ -41,7 +67,11 @@ if __name__ == "__main__":
 	parser.add_argument('-p', '--prefix', action = 'store', type = str, 
 		dest = 'PREFIX', default="ISEC_OUTPUT", 
 		help = "prefix to store output (i.e., output folder)")
+	parser.add_argument('--all', action = 'store_true', 
+		dest = "ISEC_ALL", default = False,
+		help = "intersect each file against all files in input")
+	
 	args = parser.parse_args()
 	
-	main(args.INPUT_FILE, args.PREFIX)	
+	main(args.INPUT_FILE, args.PREFIX, args.ISEC_ALL)	
 
