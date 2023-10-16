93f76a05ad2c4e651125b1081fa958f963bf7df8
Matthew S Rich <matt.rich@utah.edu>
2023-10-15 19:07:06 -0600

    split off all the parallel for gatk into a new script. implemented threads as --native-pair-hmm-threads in call_variants_gatk

--- call_variants_gatk_parallel.sh
+++ call_variants_gatk_parallel.sh
@@ -10,7 +10,11 @@ HELP(){
 		echo "-a, --alignments	BAM or SAM file containing alignments"
 		echo "-g, --genome	FASTA file for reference genome"
 		echo "optional argument:"
+		echo "--parallel	parallelize basecalling with gnu parallel"
+		echo "		argument: window size for chunking genome"
+		echo "			REQUIRES: parallel, bedtools, genome faidx"
 		echo "-t, --threads	number of threads"
+		echo "--gvcf		output GVCF instead of VCF"
 		echo "--tmpdir	directory for temporary files"
 		echo 
         echo "syntax: call_variants_gatk.sh [BAM/SAM] [FASTA]"
@@ -39,6 +43,10 @@ while [ $# -gt 0 ]; do
 						GVCF=1
 						shift 1
 						;;
+				--parallel)
+						PARALLEL="$2"
+						shift 2
+						;;
 				--tmpdir)
 						_TMPDIR="$2"
 						shift 2
@@ -65,16 +73,40 @@ fi
 #PARALLEL=0
 #setting threads=1 still splits into multiple files, 
 #which runs relatively quickly (~10min?)
-#THREADS=1
+THREADS=1
 
 #then perform basecalling 
 echo "alignments: ${ALIGNMENTS}"
 echo "genome: ${GENOME}"
+if [ ${PARALLEL} -ne 0 ]; then
+	echo "parallel threads: ${THREADS}"
+	echo "window size: ${PARALLEL}"
+fi
 
-if [ $GVCF -eq 0 ]; then
-	gatk HaplotypeCaller -I ${ALIGNMENTS} -O ${_NAME}.snp.vcf.gz -R ${GENOME} --native-pair-hmm-threads ${THREADS}
-else
-	gatk HaplotypeCaller -I ${ALIGNMENTS} -O ${_NAME}.g.vcf.gz -R ${GENOME} -ERC GVCF --native-pair-hmm-threads ${THREADS}
+if [ $PARALLEL -ne 0 ]; then
+	if [ -d ${_TMPDIR} ]; then
+			rm -rf ${_TMPDIR}
+	fi
+
+	mkdir ${_TMPDIR}
+	bedtools makewindows -g ${GENOME}.fai -w ${PARALLEL} | bioawk -t '{print $1 ":" $2+1 "-" $3}' > ${_TMPDIR}/tmp.windows
+
+	if [ $GVCF -eq 0 ]; then	
+		parallel --jobs ${THREADS} gatk HaplotypeCaller -R ${GENOME} -I ${ALIGNMENTS} -O ${_TMPDIR}/tmp.{#}.vcf.gz --intervals {1} :::: ${_TMPDIR}/tmp.windows
+		#then need to merge region'd vcfs
+		bcftools concat -a --threads ${THREADS} -Oz ${_TMPDIR}/tmp.*.vcf.gz | bcftools sort -Oz -o ${_NAME}.snp.vcf.gz
+	else
+		parallel --jobs ${THREADS} gatk HaplotypeCaller -R ${GENOME} -I ${ALIGNMENTS} -O ${_TMPDIR}/tmp.{#}.vcf.gz -ERC GVCF --intervals {1} :::: ${_TMPDIR}/tmp.windows
+		#then need to merge region'd vcfs
+		bcftools concat -a --threads ${THREADS} -Oz ${_TMPDIR}/tmp.*.vcf.gz | bcftools sort -Oz -o ${_NAME}.g.vcf.gz
+	fi
+	rm -rf ${_TMPDIR}
+else 
+	if [ $GVCF -eq 0 ]; then
+		gatk HaplotypeCaller -I ${ALIGNMENTS} -O ${_NAME}.snp.vcf.gz -R ${GENOME}
+	else
+		gatk HaplotypeCaller -I ${ALIGNMENTS} -O ${_NAME}.g.vcf.gz -R ${GENOME} -ERC GVCF
+	fi
 fi
 
 ###index VCF w/ GATK IndexFeatureFile for gVCF
